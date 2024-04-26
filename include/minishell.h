5abd93b9509da58da8e279f9e4ff561bd339172a
y-syo <mmoussou@student.42angouleme.fr>
2024-04-26 13:54:10 +0200

    「✨」 feat: added include and header file

---
+++ include/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmoussou <mmoussou@student.42angouleme.fr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 09:38:51 by mmoussou          #+#    #+#             */
+/*   Updated: 2024/04/26 10:07:35 by mmoussou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <readline/readline.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include "libft.h"
+
+void	get_env(char **env);
+
+#endif
