77786466e37063f44adf9d9494bf94bbb079d958
y-syo <mmoussou@student.42angouleme.fr>
2024-04-29 13:06:19 +0200

    「🏗️」 wip(minishell.h): added env things to the header

--- include/minishell.h
+++ include/minishell.h
@@ -6,18 +6,29 @@
 /*   By: mmoussou <mmoussou@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 09:38:51 by mmoussou          #+#    #+#             */
-/*   Updated: 2024/04/26 10:07:35 by mmoussou         ###   ########.fr       */
+/*   Updated: 2024/04/29 13:02:46 by mmoussou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "parsing.h"
+
 # include <readline/readline.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include "libft.h"
 
-void	get_env(char **env);
+typedef struct s_env
+{
+	char			*name;
+	char			*content;
+	struct s_env	*next;
+}	t_env;
+
+//
+
+char	set_env(char **env, const char *name, char *content);
 
 #endif
