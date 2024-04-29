9c0be09aa1ad02a3d3aa0cbeff37d424be13081a
y-syo <mmoussou@student.42angouleme.fr>
2024-04-29 13:55:21 +0200

    「✨」 feat(headers): organized header files

--- include/execution.h
+++ include/execution.h
@@ -1,26 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   execution.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmoussou <mmoussou@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/26 09:38:51 by mmoussou          #+#    #+#             */
-/*   Updated: 2024/04/29 13:21:45 by mmoussou         ###   ########.fr       */
+/*   Created: 2024/04/29 13:20:22 by mmoussou          #+#    #+#             */
+/*   Updated: 2024/04/29 13:21:31 by mmoussou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef EXECUTION_H
+# define EXECUTION_H
 
-# include "parsing.h"
-# include "execution.h"
-
-# include <readline/readline.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include "libft.h"
-
-char	set_env(char **env, const char *name, char *content);
+typedef struct s_env
+{
+	char			*name;
+	char			*content;
+	struct s_env	*next;
+}	t_env;
 
 #endif
