bc5af9f056cc3126120e414a0c7b1c623c070f1f
adjoly <adamjly@proton.me>
2024-05-21 21:03:03 +0200

    「🔨」 fix(Issue : prompt/historique #3): Goofy indent removed

--- src/prompt/get_prompt.c
+++ src/prompt/get_prompt.c
@@ -6,7 +6,7 @@
 /*   By: adjoly <adjoly@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 13:25:42 by adjoly            #+#    #+#             */
-/*   Updated: 2024/05/21 20:48:02 by adjoly           ###   ########.fr       */
+/*   Updated: 2024/05/21 21:02:18 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,18 @@ char	*get_prompt(void)
 		prompt = ft_strdup("nixos");
 	prompt = ft_strjoin(prompt, "@");
 	if (!prompt)
-						return (NULL);
+		return (NULL);
 	prompt = ft_strjoin_free(prompt, get_hostname());
 	if (!prompt)
-						return (NULL);
+		return (NULL);
 	prompt = ft_strjoin_free_s1(prompt, ":");
 	if (!prompt)
-						return (NULL);
+		return (NULL);
 	prompt = ft_strjoin_free(prompt, get_pwd());
 	if (!prompt)
-						return (NULL);
+		return (NULL);
 	prompt = ft_strjoin_free_s1(prompt, "$ ");
 	if (!prompt)
-						return (NULL);
+		return (NULL);
 	return (prompt);
 }
