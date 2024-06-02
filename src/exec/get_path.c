06f2480711fb7a8581a3cfb087345ff514fa44aa
yosyo <mmoussou@student.42angouleme.fr>
2024-06-02 21:18:42 +0200

    Revert "「✨」 feat(exec_cmd): can now execute commands ! still kind of a wip tho"
    This reverts commit 32e09ff98a41d05937627c5a302c430c91220474.

--- src/exec/get_path.c
+++ src/exec/get_path.c
@@ -6,19 +6,18 @@
 /*   By: mmoussou <mmoussou@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 01:42:17 by mmoussou          #+#    #+#             */
-/*   Updated: 2024/06/02 21:05:25 by mmoussou         ###   ########.fr       */
+/*   Updated: 2024/05/19 04:33:52 by mmoussou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	get_path_list(char *path, t_list *list_entry)
+int	get_path_list(char *path)
 {
 	DIR				*path_dir;
 	struct dirent	*dir_entry;
 	char			*abs_path;
 	struct stat		entry;
-	t_list			*new_list_entry;
 
 	path_dir = opendir(path);
 	if (!path_dir)
@@ -27,64 +26,41 @@ int	get_path_list(char *path, t_list *list_entry)
 	while (dir_entry)
 	{
 		if (!ft_strncmp(dir_entry->d_name, ".", ft_strlen(dir_entry->d_name))
-			|| !ft_strncmp(dir_entry->d_name, "..",
-				ft_strlen(dir_entry->d_name)))
+			|| !ft_strncmp(dir_entry->d_name, "..", ft_strlen(dir_entry->d_name)))
 		{
 			dir_entry = readdir(path_dir);
 			continue ;
 		}
-		abs_path = ft_calloc(sizeof(char), strlen(path)
-				+ strlen(dir_entry->d_name) + 2);
+		abs_path = ft_calloc(sizeof(char), strlen(path) + strlen(dir_entry->d_name) + 1);
 		if (!abs_path)
 			return (-1);
 		strcat(abs_path, path);
-		strcat(abs_path, "/");
 		strcat(abs_path, dir_entry->d_name);
 		stat(abs_path, &entry);
 		if (S_ISREG(entry.st_mode)
 			&& (entry.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)))
-		{
-			if (list_entry->content == NULL)
-				list_entry->content = abs_path;
-			else
-			{
-				new_list_entry = ft_lstnew(abs_path);
-				if (!new_list_entry)
-					return (-1);
-				ft_lstadd_back(&list_entry, new_list_entry);
-			}
-		}
+			printf("%s\n", dir_entry->d_name);
+		free(abs_path);
 		dir_entry = readdir(path_dir);
 	}
 	closedir(path_dir);
 	return (0);
 }
 
-char	**get_path(char *path)
+int	get_path(char *path)
 {
 	char	**path_dir;
-	char	**path_list;
-	t_list	*list_entry;
 	int		i;
 
-	list_entry = ft_lstnew(NULL);
 	path_dir = ft_split(path, ':');
 	if (!path_dir)
-		return (NULL);
+		return (-1);
 	i = 0;
 	while (path_dir[i])
 	{
-		get_path_list(path_dir[i], list_entry);
+		get_path_list(path_dir[i]);
 		i++;
 	}
-	i = 0;
-	path_list = ft_calloc(sizeof(char *), ft_lstsize(list_entry) + 1);
-	while (list_entry)
-	{
-		path_list[i] = list_entry->content;
-		list_entry = list_entry->next;
-		i++;
-	}
-	ft_free("al", &path_dir, &list_entry);
-	return (path_list);
+	ft_free("a", &path_dir);
+	return (0);
 }
