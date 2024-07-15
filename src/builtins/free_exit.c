9a5ff21103b824e4dcc01448b096a0850f03dac2
Adam Joly <adjoly@student.42angouleme.fr>
2024-07-15 15:27:59 +0200

    「🔨」 fix: compiling

--- src/builtins/free_exit.c
+++ src/builtins/free_exit.c
@@ -6,13 +6,16 @@
 /*   By: adjoly <adjoly@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 12:28:36 by adjoly            #+#    #+#             */
-/*   Updated: 2024/07/15 15:24:37 by adjoly           ###   ########.fr       */
+/*   Updated: 2024/07/15 15:27:45 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "minishell.h"
+#include "parsing.h"
 
 void	free_exit(t_env *env)
 {
 	rl_clear_history();
-	ft_envclear(&env_l, free);
+	ft_envclear(&env, free);
 	ft_lstclear(get_list(NULL), &free_cmd);
 }
