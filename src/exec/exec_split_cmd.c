f22d5653d8def54ef451d4ed9d8a829fce286c1c
yosyo <mmoussou@student.42angouleme.fr>
2024-08-09 19:06:14 +0200

    「🔨」 fix: aled

--- src/exec/exec_split_cmd.c
+++ src/exec/exec_split_cmd.c
@@ -6,7 +6,7 @@
 /*   By: mmoussou <mmoussou@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 14:55:06 by mmoussou          #+#    #+#             */
-/*   Updated: 2024/08/01 16:38:31 by mmoussou         ###   ########.fr       */
+/*   Updated: 2024/08/09 18:59:15 by mmoussou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ int	exec_fork_cmd(t_cmd *cmd, char **env, t_env *env_t, int pipe_fd[2])
 	if (!fork_pid)
 	{
 		__fork_single_cmd(cmd, env, env_t, exec);
-		free_exec(env_t, env);
+		free_exit(env_t, env);
 		exit(get_exit_code(-1));
 	}
 	return (fork_pid);
@@ -105,7 +105,7 @@ t_exec	exec_pipe(t_exec exec, t_list *list_cmd, t_env *env)
 			((t_cmd *)(list_cmd->next->content))->infile = exec.pipe_fd[0];
 		exec.status = exec_fork_cmd(list_cmd->content, exec.env_array, \
 			env, exec.pipe_fd);
-		__close(list_cmd->content, exec.pipe_fd[0], exec.pipe_fd[1]);
+		__close(list_cmd->content, -1, -1);
 		if (exec.status != -1)
 			exec.i++;
 		list_cmd = list_cmd->next;
