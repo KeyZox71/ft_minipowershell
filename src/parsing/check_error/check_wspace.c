07cd9771bf8198a1bb2f49baf4163c1bc2766bed
Adam Joly <adjoly@student.42angouleme.fr>
2024-08-14 10:19:39 +0200

    「🔨」 fix: fixed some things.

--- src/parsing/check_error/check_wspace.c
+++ src/parsing/check_error/check_wspace.c
@@ -6,7 +6,7 @@
 /*   By: adjoly <adjoly@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 19:18:19 by adjoly            #+#    #+#             */
-/*   Updated: 2024/07/30 14:36:47 by adjoly           ###   ########.fr       */
+/*   Updated: 2024/08/14 09:58:59 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ bool	check_wspace(char *readline)
 	while (*readline && ft_isspace(*readline))
 		readline++;
 	if (!*readline)
-		return (send_error_parsing("Only white space in line"));
+		return (true);
 	return (false);
 }
